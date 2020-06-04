8f95170b5b713b95a77b1062cadc6d80d014575c
A G <developer0806@gmail.com>
2020-06-04 23:32:27 +0530

    - highlight desired cell - capture key events

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import pygame
-import logging
 
 
 def main():
-    global window, width, color, cellWidth, box_start_pos
     initialize_game()
-    redraw()
-    pygame.time.delay(5000)
+    count = 5
+
+    while who_won == "none":
+        event = pygame.event.poll()
+        if event.type == pygame.KEYUP:
+            key = pygame.key.name(event.key)
+            print("Key is [" + key + "]")
+            count -= 1
+            if count == 0:
+                break
+
+        redraw()
+    pygame.time.delay(2000)
 
 
 def initialize_game():
-    global box_start_pos, width, cellWidth, color, window
+    global window, width, color, cellWidth, box_start_pos, who_won, box_start_pos, width, cellWidth, color, window
+    who_won = "none"
     pygame.init()
     box_start_pos = (1, 1)
     width = 300
@@ -26,18 +36,27 @@ def draw_matrix():
     pygame.draw.line(window, color, (0, cellWidth * 2), (width - 1, cellWidth * 2))
 
 
-def drawX_in_cell(param):
-    pygame.display.set_caption("This is text")
-    font = pygame.font.Font('freesansbold.ttf', 80)
-    text = font.render('O', True, color, (0, 0, 0))
-    textRect = text.get_rect()
+def draw_highlighted_cell(param):
     start = get_start_cell(param)
     end = get_end_cell(param)
-    print("X = [(" + str(start) + ")]  Y = [" + str(end) + "]")
-    textRect.center = (((end[0] - start[0]) // 2) + (param[0] * cellWidth),
-                       ((end[1] - start[1]) // 2) + (param[1] * cellWidth))
-    print("centre is [" + str(textRect.center) + "]")
-    window.blit(text, textRect)
+    pygame.draw.rect(window, (255, 255, 0), (start, (cellWidth, cellWidth)), 2)
+
+
+def draw_char_in_cell(param, char):
+    pygame.display.set_caption("This is text")
+    font = pygame.font.Font('freesansbold.ttf', 80)
+    text = font.render(char, True, color, (0, 0, 0))
+    text_rect = text.get_rect()
+    text_rect.center = get_center(param)
+    window.blit(text, text_rect)
+
+
+def get_center(param):
+    start = get_start_cell(param)
+    end = get_end_cell(param)
+    # print("X = [(" + str(start) + ")]  Y = [" + str(end) + "]")
+    return (((end[0] - start[0]) // 2) + (param[0] * cellWidth),
+            ((end[1] - start[1]) // 2) + (param[1] * cellWidth))
 
 
 def get_end_cell(param):
@@ -51,12 +70,13 @@ def get_start_cell(param):
 def redraw():
     draw_border()
     draw_matrix()
-    drawX_in_cell((1, 1))
+    cell = (0, 0)
+    draw_highlighted_cell(cell)
+    draw_char_in_cell(cell, "X")
     pygame.display.update()
 
 
 def draw_border():
-    logging.debug("width [" + str(width) + "] cell_width [" + str(cellWidth) + "]")
     pygame.draw.rect(window, color, (box_start_pos, ((width - 2), (width - 2))), 2)
 
 
